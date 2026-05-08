a705fa6f9ef1b9954f8c67b21dceb224959ac777
zaltemohit9-glitch <zaltemohit9@gmail.com>
2026-05-09 00:32:39 +0530

    assignment 7-1

---
+++ mydata7-1 (1).php
@@ -0,0 +1,18 @@
+<?php
+$student = [
+    [
+        "id" => 1,
+        "name" => "Mohit"
+    ],
+    [
+        "id" => 2,
+        "name" => "Krushna"
+    ],
+    [
+        "id" => 3,
+        "name" => "Khushal"
+    ]
+];
+
+print_r(json_encode($student));
+?>
